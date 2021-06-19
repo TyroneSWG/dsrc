bce2d342795722203dcbaee8b4584f138650b9da
Roachie <freddylikespotatos@gmail.com>
2021-06-19 16:27:56 -0500

    Medical building elevator.

---
+++ sku.0/sys.server/compiled/game/script/theme_park/stp/elevator_medical_office.java
@@ -0,0 +1,37 @@
+package script.theme_park.stp;
+
+import script.*;
+
+public class elevator_medical_office extends base_class {
+
+    public elevator_medical_office() {
+    }
+    public static String BUILDING = "object/building/player/player_house_generic_small_windowed.iff";
+    public int OnAttach(obj_id self) throws InterruptedException {
+        return SCRIPT_CONTINUE;
+    }
+
+    public int OnInitialized(obj_id self) throws InterruptedException {
+        return SCRIPT_CONTINUE;
+    }
+    public int OnObjectMenuRequest(obj_id self, obj_id player, menu_info mi) throws InterruptedException
+    {
+        menu_info_data data = mi.getMenuItemByType(menu_info_types.ITEM_USE);
+        if (data != null)
+        {
+            data.setServerNotify(true);
+        }
+        return SCRIPT_CONTINUE;
+    }
+    public int OnObjectMenuSelect(obj_id self, obj_id player, int item) throws InterruptedException
+    {
+        if (item == menu_info_types.ITEM_USE)
+        {
+            //@TODO make a better method for these types of transport
+            obj_id[] targetLocs = getAllObjectsWithTemplate(getLocation(self), 120.0f, BUILDING);
+            obj_id cell = getCellId(targetLocs[0], "room1");
+            warpPlayer(player, getCurrentSceneName(), 0.0f, 0.0f, 0.0f, cell, 0.0f, 0.0f, 0.0f);
+        }
+        return SCRIPT_CONTINUE;
+    }
+}
